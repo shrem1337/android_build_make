4dd5fc05c655c6d0d40c5650a102192f55153d01
ezio84 <brabus84@gmail.com>
2017-08-25 10:26:20 +0200

    brunch/breakfast: set userdebug
    Change-Id: Icba3c57f1d47a6b92b953d9b4db2897bc4928d7d
    Signed-off-by: spezi77 <spezi77@gmx.com>

--- envsetup.sh
+++ envsetup.sh
@@ -622,7 +622,7 @@ function breakfast()
         else
             # This is probably just the model name
             if [ -z "$variant" ]; then
-                variant="user"
+                variant="userdebug"
             fi
             lunch $target-$variant
         fi
