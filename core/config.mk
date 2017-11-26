62cf9556bb6a9fa927768672e10c37327d29fe26
spezi77 <spezi77@gmx.com>
2017-11-26 03:35:51 +0100

    build: Target GCC version to 7.x [1/2].
    Signed-off-by: spezi77 <spezi77@gmx.com>

--- core/config.mk
+++ core/config.mk
@@ -372,9 +372,9 @@ TARGET_CPU_ABI_LIST_32_BIT := $(subst $(space),$(comma),$(strip $(TARGET_CPU_ABI
 TARGET_CPU_ABI_LIST_64_BIT := $(subst $(space),$(comma),$(strip $(TARGET_CPU_ABI_LIST_64_BIT)))
 
 # GCC version selection
-TARGET_GCC_VERSION := 4.9
+TARGET_GCC_VERSION := 7.x
 ifdef TARGET_2ND_ARCH
-2ND_TARGET_GCC_VERSION := 4.9
+2ND_TARGET_GCC_VERSION := 7.x
 endif
 TARGET_LEGACY_GCC_VERSION := 4.8
 
