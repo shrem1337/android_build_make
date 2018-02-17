rrajesh011/build

Set CUSTOM_BUILD priority above TARGET_BUILD_APPS
Even if we are doing TARGET_BUILD_APPS, we may want to use a custom
target for building.

Change-Id: I1ba99cd575caa6b3c1d166e90721c1ee2b6f18ba
Signed-off-by: mydongistiny <jaysonedson@gmail.com>

core/product_config.mk
@@ -179,19 +179,20 @@ include $(BUILD_SYSTEM)/node_fns.mk
 include $(BUILD_SYSTEM)/product.mk
 include $(BUILD_SYSTEM)/device.mk
 
-ifneq ($(strip $(TARGET_BUILD_APPS)),)
-# An unbundled app build needs only the core product makefiles.
-all_product_configs := $(call get-product-makefiles,\
-    $(SRC_TARGET_DIR)/product/AndroidProducts.mk)
+# A CUSTOM build needs only the CUSTOM product makefiles.
+ifneq ($(CUSTOM_BUILD),)
+  all_product_configs := $(shell ls vendor/benzo/products/$(CUSTOM_BUILD).mk)
 else
-  ifneq ($(CUSTOM_BUILD),)
-    all_product_configs := $(shell ls vendor/benzo/products/$(CUSTOM_BUILD).mk)
+  ifneq ($(strip $(TARGET_BUILD_APPS)),)
+  # An unbundled app build needs only the core product makefiles.
+  all_product_configs := $(call get-product-makefiles,\
+      $(SRC_TARGET_DIR)/product/AndroidProducts.mk)
   else
     # Read in all of the product definitions specified by the AndroidProducts.mk
     # files in the tree.
     all_product_configs := $(get-all-product-makefiles)
-  endif
-endif
+  endif # TARGET_BUILD_APPS
+endif # CUSTOM_BUILD
 
 all_named_products :=
 ifeq ($(CUSTOM_BUILD),)

shrem1337/android_build_make

Signed-off-by: spezi77 <spezi77@gmx.com>

core/product_config.mk
@@ -168,19 +168,20 @@ include $(BUILD_SYSTEM)/node_fns.mk
 include $(BUILD_SYSTEM)/product.mk
 include $(BUILD_SYSTEM)/device.mk
 
-ifneq ($(strip $(TARGET_BUILD_APPS)),)
-# An unbundled app build needs only the core product makefiles.
-all_product_configs := $(call get-product-makefiles,\
-    $(SRC_TARGET_DIR)/product/AndroidProducts.mk)
+# A CUSTOM build needs only the CUSTOM product makefiles.
+ifneq ($(CUSTOM_BUILD),)
+  all_product_configs := $(shell ls vendor/pixeldust/products/$(CUSTOM_BUILD).mk)
 else
-  ifneq ($(CUSTOM_BUILD),)
-    all_product_configs := $(shell ls vendor/pixeldust/products/$(CUSTOM_BUILD).mk)
+  ifneq ($(strip $(TARGET_BUILD_APPS)),)
+  # An unbundled app build needs only the core product makefiles.
+  all_product_configs := $(call get-product-makefiles,\
+      $(SRC_TARGET_DIR)/product/AndroidProducts.mk)
   else
     # Read in all of the product definitions specified by the AndroidProducts.mk
     # files in the tree.
     all_product_configs := $(get-all-product-makefiles)
-  endif
-endif
+  endif # TARGET_BUILD_APPS
+endif # CUSTOM_BUILD
 
 all_named_products :=
 ifeq ($(CUSTOM_BUILD),)